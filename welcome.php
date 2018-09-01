f889795a1978813f6d70c7f851b1c2d40658db8b
Hendrik26 <hendrik.herrdler@gmail.com>
2018-09-01 15:40:28 +0200

    nach Batenbankeinrichtung 2. Commit

--- welcome.php
+++ welcome.php
@@ -7,9 +7,28 @@
 			$zahl01 = (int)$_POST["Zahl01"]; 
 			$zahl02 = (int)$_POST["Zahl02"];
 			$zahl03 = $zahl01 + $zahl02;
-			$text = "Welcome $nn <br> Summe==$zahl03 <br> Your email address is: $em";
-			echo $text; 
-		?><br>
+			$text = "PHPStorm Test004DB <br> Welcome $nn <br> Summe==$zahl03 <br> Your email address is: $em <br> <br>";
+			echo $text;
+
+         $servername = "localhost";
+         $username = "root";
+         $password = "";
+         $database = "phpstorm001db";
+         $tablename = "table001";
+
+         try {
+             $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+             // set the PDO error mode to exception
+             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+             $sql = "INSERT INTO $tablename (Nachname,eMail,Zahl01,Zahl02) VALUES ('$nn', '$em',$zahl01,$zahl02 )";
+             $conn->exec($sql);
+             echo "New record inserted successfully";
+         } catch (PDOException $e) {
+             echo $sql . "<br>" . $e->getMessage();
+         }
+         $conn = null;
+
+         ?><br>
 		________________________________________________
 
 	</body>
